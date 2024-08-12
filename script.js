f390312b34747c116a249a88e83e42b863900fbe
Dhatchin100 <dhatchin100@gmail.com>
2024-08-12 16:48:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function fibonacci(num) {
-// your code here
+if(num==0)
+{
+	return 0;
+}
+	else if(num==1)
+	{
+		return 1
+	}
+	else 
+	{
+		let num1=0;
+		let num=2;
+		let i=2;
+		while(i<num)
+			{
+				sum=num1+num2;
+				num1=num2;
+				num2=sum;
+				i+=1;
+			}
+	}
 }
 
 module.exports = fibonacci;
