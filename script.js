a4cbb61d7765997faeb66720c74d99a17d7984e0
Dhatchin100 <dhatchin100@gmail.com>
2024-08-13 22:37:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,17 @@
 function fibonacci(num) {
-if(num==1)
-{
-	return 0;
-}
-	else if(num==2)
-	{
-		return 1
-	}
-	else 
-	{
-		let num1=0;
-		let num=2;
-		let sum=0;
-		let i=2;
-		while(i<num)
-			{
-				sum=num1+num2;
-				num1=num2;
-				num2=sum;
-				i+=1;
-			}
-		console.log(sum);
-	}
+  if (num === 1) return 0;
+  else if (num === 2) return 1;
+  num -= 2;
+  var num1 = 0;
+  var num2 = 1;
+  var sum;
+  var i = 0;
+  for (i = 0; i < num; i++) {
+    sum = num1 + num2;
+    num1 = num2;
+    num2 = sum;
+  }
+  return num2;
 }
 
 module.exports = fibonacci;
